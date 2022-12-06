6a557d2871c6694cc341f2900e6e8d9f8468fd72
Jordan <jwald3@brockport.edu>
2022-12-06 00:54:32 -0500

    Created initial fetch for Pokemon data

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        height: "100%",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type BasePokemonResponse = {
+    name: string;
+    url: string;
+};
+
+type UpdatedPokemonResponse = {
+    name: string;
+    url: string;
+    image_url: string;
+};
 
 const HomeScreen = () => {
+    const [pokemonList, setPokemonList] = useState<Array<BasePokemonResponse>>(
+        []
+    );
+    const [updatedPokemonList, setUpdatedPokemonList] = useState<
+        Array<UpdatedPokemonResponse>
+    >([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(
+                "https://pokeapi.co/api/v2/pokemon"
+            );
+
+            setPokemonList(response.data.results);
+        };
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        pokemonList.forEach((pokemon) =>
+            axios.get(pokemon.url).then((data) => {
+                setUpdatedPokemonList((prev) => [
+                    ...prev,
+                    {
+                        name: pokemon.name,
+                        url: pokemon.url,
+                        image_url: data.data.sprites.front_default,
+                    },
+                ]);
+            })
+        );
+    }, [pokemonList]);
+
     return (
         <View>
-            <Text>HomeScreen</Text>
+            {updatedPokemonList.map((pokemon) => (
+                <View key={pokemon.name}>
+                    <Text>{pokemon.name}</Text>
+                    <Image
+                        source={{ uri: pokemon.image_url }}
+                        style={{ height: 50, width: 50 }}
+                    />
+                </View>
+            ))}
         </View>
     );
 };
