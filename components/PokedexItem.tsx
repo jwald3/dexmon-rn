054bc63a257a2f47e228fe9e038516b5f78902ed
Jordan <jwald3@brockport.edu>
2022-12-16 19:10:20 -0500

    Updated import for PokedexItem

--- components/PokedexItem.tsx
+++ components/PokedexItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Image, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { UpdatedPokemonResponse } from "../screens/HomeScreen";
+import { UpdatedPokemonResponse } from "../screens/Home";
 import { ChevronRightIcon } from "react-native-heroicons/outline";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
