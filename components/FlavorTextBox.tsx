29639ad22c5bc252b53321ab174a300be84c29bf
Jordan <jwald3@brockport.edu>
2022-12-20 21:41:03 -0500

    Extracted styles from FlavorTextBox

--- components/FlavorTextBox.tsx
+++ components/FlavorTextBox.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 interface FlavorTextProps {
@@ -7,50 +7,12 @@ interface FlavorTextProps {
 
 const FlavorTextBox = ({ text }: FlavorTextProps) => {
     return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: "center",
-                marginHorizontal: 5,
-                borderWidth: 1,
-                borderColor: "rgba(255,255,255,0.50)",
-                borderRadius: 3,
-                width: "100%",
-            }}
-        >
-            <View
-                style={{
-                    backgroundColor: "#42AD4A",
-                    width: "100%",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    paddingVertical: 3,
-                }}
-            >
-                <Text
-                    style={{
-                        fontWeight: "bold",
-                        color: "#F8F8FF",
-                    }}
-                >
-                    Flavor Text
-                </Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>Flavor Text</Text>
             </View>
-            <View
-                style={{
-                    flex: 1,
-                    width: "100%",
-                    paddingHorizontal: 10,
-                    justifyContent: "center",
-                    backgroundColor: "rgba(255,255,255,0.10)",
-                }}
-            >
-                <Text
-                    style={{
-                        color: "#fff",
-                        textAlign: "center",
-                    }}
-                >
+            <View style={styles.containerBody}>
+                <Text style={styles.containerText}>
                     {text.split("\n").join(" ").replace("\f", " ")}
                 </Text>
             </View>
@@ -59,3 +21,37 @@ const FlavorTextBox = ({ text }: FlavorTextProps) => {
 };
 
 export default FlavorTextBox;
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        alignItems: "center",
+        marginHorizontal: 5,
+        borderWidth: 1,
+        borderColor: "rgba(255,255,255,0.50)",
+        borderRadius: 3,
+        width: "100%",
+    },
+    headerContainer: {
+        backgroundColor: "#42AD4A",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 3,
+    },
+    headerText: {
+        fontWeight: "bold",
+        color: "#F8F8FF",
+    },
+    containerBody: {
+        flex: 1,
+        width: "100%",
+        paddingHorizontal: 10,
+        justifyContent: "center",
+        backgroundColor: "rgba(255,255,255,0.10)",
+    },
+    containerText: {
+        color: "#fff",
+        textAlign: "center",
+    },
+});
